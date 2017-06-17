9fdb7043ea6649a081e51c90b87b1f574cbc3425
zabador <zabador2000@gmail.com>
2017-06-16 22:47:09 -0400

    Initial commit

---
+++ app/src/main/java/net/zabador/retrovault/GameListActivity.kt
@@ -0,0 +1,12 @@
+package net.zabador.retrovault
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class GameListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_list)
+    }
+}
